0317a0d8464c99aec8c38589ab75f702305c5293
Arman Ul Alam <arman.u.alam@gmail.com>
2021-02-12 01:35:32 +0600

    Formik installed

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,28 +1,38 @@
 import React, {useState} from "react";
+import {withFormik} from "formik";
 
 
-const Register = () => {
-    const [firstName, setFirstName] = useState('');
-
-    const handleChange = (e) => {
-        setFirstName(e.target.value);
-    }
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(firstName);
-    }
+const Register = ({values, handleChange, handleSubmit, handleBlur, isSubmitting}) => {
 
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="firstName">First Name</label>
-                <input type="text" name='firstName' value={firstName } onChange={handleChange}/>
-                <button type="submit" >Submit</button>
+                <input type="text"
+                       name='firstName'
+                       value={values.firstName }
+                       onChange={handleChange}
+                       onBlur={handleBlur}
+                />
+                <button disabled={isSubmitting} type="submit" >Submit</button> // disable button during form submission
             </form>
         </>
     );
 }
 
-export default Register;
+
+
+// Formik is a higher order component
+export default withFormik({
+    mapPropsToValues(){
+        return {
+            firstName: '' // returning initial state value
+        }
+    },
+    handleSubmit(values, {resetForm, setSubmitting}) {
+        setTimeout( () => {
+            setSubmitting(false);
+            console.log(values);
+        }, 1000);
+    }
+})(Register); // Receiving component as an argument and return with some extra capabilities
