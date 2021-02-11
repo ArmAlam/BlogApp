a59c837062385554ee6714092cf6deb46f95a33a
Arman Ul Alam <arman.u.alam@gmail.com>
2021-02-11 10:24:00 +0600

    Register form created

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 const Register = () => {
+    const [firstName, setFirstName] = useState('');
+
+    const handleChange = (e) => {
+        setFirstName(e.target.value);
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(firstName);
+    }
+
     return (
         <>
-            <p>Register</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <input type="text" name='firstName' value={firstName } onChange={handleChange}/>
+                <button type="submit" >Submit</button>
+            </form>
         </>
     );
 }
